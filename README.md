5629693d56f55e8e4a305caac37bfaed7b601f58
Mateus Ferreira <mateus03ferreira04@gmail.com>
2023-11-10 23:15:59 -0300

    feature-init-command

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nightmare
-Nightmare is a framework for Go pogramming language. It creates new go projects quickly, increasing productivity. Nightmare creates hexagonal architecture projects for golang.
+Nightmare is a framework for Go pogramming language. It creates new go projects quickly, increasing productivity. Nightmare creates hexagonal architecture projects for golang. [DEVELOPMENT]
---
+++ cmd/init.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Starts a new project",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var packageName string
+		var err error
+		if len(args) == 1 {
+			packageName = args[0]
+		} else {
+			packageName, err = os.Getwd()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			packageName = filepath.Base(packageName)
+		}
+
+		if err := exec.Command("go", "mod", "init", packageName).Run(); err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Go module created [%s]!\n", packageName)
+
+		const defaultPerm = 0755
+		os.Mkdir("build", defaultPerm)
+		os.Mkdir("cmd", defaultPerm)
+		os.Mkdir("configs", defaultPerm)
+		os.Mkdir("deployments", defaultPerm)
+		os.Mkdir("docs", defaultPerm)
+		os.Mkdir("examples", defaultPerm)
+		os.Mkdir("internal", defaultPerm)
+		os.MkdirAll("pkg", defaultPerm)
+		os.Mkdir("test", defaultPerm)
+		os.MkdirAll("internal/controller", defaultPerm)
+		os.MkdirAll("internal/domain", defaultPerm)
+		os.MkdirAll("internal/domain/entity", defaultPerm)
+		os.MkdirAll("internal/domain/port", defaultPerm)
+		os.MkdirAll("internal/domain/service", defaultPerm)
+
+		log.Println("Default directories created!")
+		log.Println("Project initialized!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
